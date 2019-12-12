18708a86f545d7bb17fbd95aea0f46a42c7c7ca3
xMattix95 <mattisikora@gmail.com>
2019-12-12 21:57:24 +0100

    implemented

---
+++ src/main/java/pl/agh/edu/ddos/DDOS.java
@@ -0,0 +1,11 @@
+package pl.agh.edu.ddos;
+
+import org.slf4j.Logger;
+
+import net.floodlightcontroller.core.IFloodlightProviderService;
+
+public class DDOS {
+
+	protected IFloodlightProviderService floodlightProvider;
+	protected static Logger logger;
+}
