537d9f02a38690056d23f41c41485a1e7caa5bd9
AndrzejP <andrzejpencak@gmail.com>
2019-12-17 17:34:45 +0100

    working module  with calling PacketAnalyzer

---
+++ src/main/java/pl/agh/edu/dos/DoS.java
@@ -0,0 +1,90 @@
+package pl.agh.edu.dos;
+
+import java.util.Collection;
+import java.util.HashMap;
+
+import net.floodlightcontroller.core.IFloodlightProviderService;
+import java.util.ArrayList;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Map;
+
+import org.projectfloodlight.openflow.protocol.OFMessage;
+import org.projectfloodlight.openflow.protocol.OFType;
+
+import net.floodlightcontroller.core.FloodlightContext;
+import net.floodlightcontroller.core.IOFMessageListener;
+import net.floodlightcontroller.core.IOFSwitch;
+import net.floodlightcontroller.core.module.FloodlightModuleContext;
+import net.floodlightcontroller.core.module.FloodlightModuleException;
+import net.floodlightcontroller.core.module.IFloodlightModule;
+import net.floodlightcontroller.core.module.IFloodlightService;
+
+public class DoS implements IOFMessageListener, IFloodlightModule {
+
+	protected IFloodlightProviderService floodlightProvider;
+	protected static Logger logger;
+	
+	private Map<String, Integer> counterMap = new HashMap<>();
+
+	@Override
+	public String getName() {
+		return DoS.class.getSimpleName();
+	}
+
+	@Override
+	public boolean isCallbackOrderingPrereq(OFType type, String name) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isCallbackOrderingPostreq(OFType type, String name) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Collection<Class<? extends IFloodlightService>> getModuleServices() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Map<Class<? extends IFloodlightService>, IFloodlightService> getServiceImpls() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Collection<Class<? extends IFloodlightService>> getModuleDependencies() {
+		Collection<Class<? extends IFloodlightService>> l = new ArrayList<Class<? extends IFloodlightService>>();
+		l.add(IFloodlightProviderService.class);
+		return l;
+	}
+
+	@Override
+	public void init(FloodlightModuleContext context) throws FloodlightModuleException {
+		floodlightProvider = context.getServiceImpl(IFloodlightProviderService.class);
+		logger = LoggerFactory.getLogger(DoS.class);
+
+	}
+
+	@Override
+	public void startUp(FloodlightModuleContext context) throws FloodlightModuleException {
+		floodlightProvider.addOFMessageListener(OFType.PACKET_IN, this);
+		logger.info("******************* DoS Protection started **************************");
+	}
+
+	@Override
+	public Command receive(IOFSwitch sw, OFMessage msg, FloodlightContext cntx) {
+		logger.info("******************* New Packet ***************************************");
+		
+		PacketAnalyzer analyzer = new PacketAnalyzer(counterMap);
+		analyzer.packetExtract(cntx);
+		
+		return Command.CONTINUE;
+	}
+
+}
