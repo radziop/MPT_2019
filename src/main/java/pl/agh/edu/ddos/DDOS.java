f888114057658eb9ec2e4c9290518dc49bb848e8
xMattix95 <mattisikora@gmail.com>
2019-12-18 21:13:05 +0100

    poczatkowa implementacja licznika

--- src/main/java/pl/agh/edu/ddos/DDOS.java
+++ src/main/java/pl/agh/edu/ddos/DDOS.java
@@ -1,11 +1,79 @@
 package pl.agh.edu.ddos;
 
-import org.slf4j.Logger;
 
+import net.floodlightcontroller.core.FloodlightContext;
 import net.floodlightcontroller.core.IFloodlightProviderService;
+import net.floodlightcontroller.core.IOFSwitch;
+import net.floodlightcontroller.core.module.FloodlightModuleContext;
+import net.floodlightcontroller.core.module.FloodlightModuleException;
+import net.floodlightcontroller.core.module.IFloodlightModule;
 
-public class DDOS {
+import java.util.ArrayList;
+
+import org.projectfloodlight.openflow.protocol.OFMessage;
+import org.projectfloodlight.openflow.protocol.OFType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DDOS implements IOFMessageListener, IFloodlightModule {
 
 	protected IFloodlightProviderService floodlightProvider;
 	protected static Logger logger;
+	@Override
+	public void init(FloodlightModuleContext context) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void startUp(FloodlightModuleContext context) throws FloodlightModuleException {
+		floodlightProvider.addOFMessageListener(OFType.PACKET_IN, this);
+		logger.info("*******DDoS PROTECTION START *******");
+
+	}
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return DDOS.class.getSimpleName();
+	}
+
+	@Override
+	public boolean isCallbackOrderingPrereq(OFType type, String name) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isCallbackOrderingPostreq(OFType type, String name) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Collection<Class<? extends IFloodlightService>> getModuleServices() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Map<Class<? extends IFloodlightService>, IFloodlightService> getServiceImpls() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Collection<Class<? extends IFloodlightService>> getModuleDependencies() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public net.floodlightcontroller.core.IListener.Command receive(IOFSwitch sw, OFMessage msg, FloodlightContext cntx) {
+		logger.info("************* NEW PACKET IN *************");
+		PacketExtractor extractor = new PacketExtractor();
+		extractor.packetExtract(cntx);
+		return Command.CONTINUE;
+	}
+
 }
