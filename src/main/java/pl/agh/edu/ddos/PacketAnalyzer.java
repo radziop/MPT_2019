4d7e848db94653dc6dd94c43aba7122e47daff75
Mirar7 <35768175+Mirar7@users.noreply.github.com>
2019-12-19 20:57:36 +0100

    Update PacketAnalyzer.java

--- src/main/java/pl/agh/edu/ddos/PacketAnalyzer.java
+++ src/main/java/pl/agh/edu/ddos/PacketAnalyzer.java
@@ -1,6 +1,15 @@
-package pl.agh.edu.ddos;
+package pl.agh.edu.dos;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.lang.Integer;
+
 import org.projectfloodlight.openflow.protocol.OFMessage;
 import org.projectfloodlight.openflow.types.EthType;
+import org.projectfloodlight.openflow.types.IPv4Address;
+import org.projectfloodlight.openflow.types.IpProtocol;
+import org.projectfloodlight.openflow.types.TransportPort;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import net.floodlightcontroller.core.FloodlightContext;
@@ -12,46 +21,70 @@ import net.floodlightcontroller.packet.TCP;
 import net.floodlightcontroller.packet.UDP;
 
 public class PacketAnalyzer {
-	private static final Logger logger = LoggerFactory.getLogger(PacketExtractor.class);
+
+	private static final Logger logger = LoggerFactory.getLogger(PacketAnalyzer.class);
 	private FloodlightContext cntx;
 	private OFMessage msg;
 	protected IFloodlightProviderService floodlightProvider;
 	private Ethernet eth;
 	private IPv4 ipv4;
-	private ARP arp;
 	private TCP tcp;
 	private UDP udp;
 	private IPv4Address srcIP;
 	private IPv4Address dstIP;
 	private TransportPort srcPort;
 	private TransportPort dstPort;
-	private Map<String,Integer> counterMap;
-	public PacketAnalyzer(Map<String,Integer> counterMap) {
+	private short flags;
+
+	private Map<String, Integer> counterMap;
+
+	public PacketAnalyzer(Map<String, Integer> counterMap) {
 		this.counterMap = counterMap;
 	}
+
 	public void packetExtract(FloodlightContext cntx) {
 		this.cntx = cntx;
 		extractEth();
 	}
+
 	public void extractEth() {
-		eth = IFloodlightProviderService.bcStore.get(cntx,
-		IFloodlightProviderService.CONTEXT_PI_PAYLOAD);
-		logger.info("Frame: src mac {}", eth.getSourceMACAddress());
-		logger.info("Frame: dst mac {}", eth.getDestinationMACAddress());
-		logger.info("Frame: ether_type {}", eth.getEtherType());
-		if (eth.getEtherType() == EthType.ARP) {
-		arp = (ARP) eth.getPayload();
-		//extractArp();
-		}
+		eth = IFloodlightProviderService.bcStore.get(cntx, IFloodlightProviderService.CONTEXT_PI_PAYLOAD);
 		if (eth.getEtherType() == EthType.IPv4) {
-		ipv4 = (IPv4) eth.getPayload();
-		extractIp();
+			ipv4 = (IPv4) eth.getPayload();
+			extractIp();
 		}
 	}
-	pubic void extractIp() {
+
+	public void extractIp() {
+		if (ipv4.getProtocol() == IpProtocol.TCP) {
+			srcIP = ipv4.getSourceAddress();
+			dstIP = ipv4.getDestinationAddress();
+
+			tcp = (TCP) ipv4.getPayload();
+			extractTCP();
+		}
 	}
-	pubic void extractTCP() {
+
+	public void extractTCP() {
+		srcPort = tcp.getSourcePort();
+		dstPort = tcp.getDestinationPort();
+		flags = tcp.getFlags();
+		flowCounter();
 	}
+
 	public void flowCounter() {
+
+		if (dstIP.toString().equals("10.0.0.3") && flags == 2) { // if SYN flag set
+			if (counterMap.containsKey(srcIP.toString()+":"+srcPort.toString())) {
+				counterMap.put(srcIP.toString()+":"+srcPort.toString(), counterMap.get(srcIP.toString()+":"+srcPort.toString()) + 1);
+			} else {
+				counterMap.put(srcIP.toString()+":"+srcPort.toString(), 1);
+			}
+			String logMessage = "New flow: Source IP/Port: " + srcIP.toString()+":"+srcPort.toString() + " Connections counter: "
+					+ counterMap.get(srcIP.toString()+":"+srcPort.toString()).toString();
+			logger.info("{}", logMessage);
+		}
+
 	}
+
 }
