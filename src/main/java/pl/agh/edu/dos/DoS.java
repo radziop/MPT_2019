8ed559d21c1bd259434e46d8af8cc0343af352e1
kamilkryczka <kamilkryczka96@gmail.com>
2020-01-08 18:10:58 +0100

    blocking attacking hst

--- src/main/java/pl/agh/edu/dos/DoS.java
+++ src/main/java/pl/agh/edu/dos/DoS.java
@@ -3,17 +3,23 @@ package pl.agh.edu.dos;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
-
-import net.floodlightcontroller.core.IFloodlightProviderService;
+import java.util.Map;
 import java.util.ArrayList;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.Map;
-
+import org.projectfloodlight.openflow.types.IPv4Address;
+import org.projectfloodlight.openflow.types.EthType;
+import org.projectfloodlight.openflow.types.OFBufferId;
 import org.projectfloodlight.openflow.protocol.OFMessage;
 import org.projectfloodlight.openflow.protocol.OFType;
+import org.projectfloodlight.openflow.protocol.match.Match;
+import org.projectfloodlight.openflow.protocol.match.MatchField;
+import org.projectfloodlight.openflow.protocol.OFFlowMod;
+import org.projectfloodlight.openflow.protocol.action.OFAction;
 
+import net.floodlightcontroller.core.IFloodlightProviderService;
 import net.floodlightcontroller.core.FloodlightContext;
 import net.floodlightcontroller.core.IOFMessageListener;
 import net.floodlightcontroller.core.IOFSwitch;
@@ -21,17 +27,18 @@ import net.floodlightcontroller.core.module.FloodlightModuleContext;
 import net.floodlightcontroller.core.module.FloodlightModuleException;
 import net.floodlightcontroller.core.module.IFloodlightModule;
 import net.floodlightcontroller.core.module.IFloodlightService;
+import net.floodlightcontroller.util.FlowModUtils;
 
 public class DoS implements IOFMessageListener, IFloodlightModule {
 
 	protected IFloodlightProviderService floodlightProvider;
 	protected static Logger logger;
 	
-	private Map<String, Integer> counterMap = new HashMap<>();
-	//private Map<String, List<Integer>> srcPortMap = new HashMap<>();
-	//List<Integer> portList = new ArrayList<>();
-	private Integer simultaneousConnectionThreshold = 20;
+	private Map<IPv4Address, Integer> counterMap = new HashMap<>();
+	private IPv4Address current_ip;
+	private Integer simultaneousConnectionThreshold = 4;
 	private Integer blockingTime = 30;
+	private List<IPv4Address> blocked_hosts = new ArrayList<>();
 
 	@Override
 	public String getName() {
@@ -86,10 +93,57 @@ public class DoS implements IOFMessageListener, IFloodlightModule {
 	public Command receive(IOFSwitch sw, OFMessage msg, FloodlightContext cntx) {
 		//logger.info("******************* New Packet ***************************************");
 		
-		PacketAnalyzer analyzer = new PacketAnalyzer(counterMap, simultaneousConnectionThreshold, blockingTime);
+		PacketAnalyzer analyzer = new PacketAnalyzer(counterMap);
 		analyzer.packetExtract(cntx);
+		
+		for(IPv4Address ip: counterMap.keySet()) {
+			current_ip = ip;
+			if (counterMap.get(current_ip) > simultaneousConnectionThreshold) {
+				if (!blocked_hosts.contains(current_ip)){
+					blockHostByIpAddress(sw);
+				}
+			}
+		}
 		
 		return Command.CONTINUE;
 	}
 
+	public void blockHostByIpAddress(IOFSwitch sw) {
+        Match.Builder mb = sw.getOFFactory().buildMatch();
+			mb.setExact(MatchField.ETH_TYPE, EthType.IPv4);
+			mb.setExact(MatchField.IPV4_SRC, current_ip);
+			Match m = mb.build();
+
+        OFFlowMod.Builder fmb = sw.getOFFactory().buildFlowAdd();
+        List<OFAction> actions = new ArrayList<>();
+
+        fmb.setHardTimeout(0)
+        .setIdleTimeout(120)
+        .setBufferId(OFBufferId.NO_BUFFER) 
+        .setMatch(m)
+        .setPriority(Integer.MAX_VALUE);
+        FlowModUtils.setActions(fmb, actions, sw);
+        sw.write(fmb.build());
+		
+        blocked_hosts.add(current_ip);
+		String logMessage = "Host with source IP: " + current_ip.toString() + " blocked for " + blockingTime + " seconds!";
+		logger.info("{}", logMessage);
+		
+		// Schedule unblocking the host after 'blockingTime' timer expires
+		new java.util.Timer().schedule(new java.util.TimerTask() {
+			@Override
+		    public void run() {unblockHostByIpAddress();}
+		}, blockingTime*1000);
+	}
+	
+	public void unblockHostByIpAddress() {
+//		String command = "ovs-ofctl del-flows s1 \"nw_src=" + current_ip + "\"";
+//		OFFlowMod.Builder fmb = sw.getOFFactory().buildFlowDelete();
+		
+		
+		blocked_hosts.remove(current_ip);
+		String logMessage = "Host with source IP: " + current_ip.toString() + " unblocked.";
+		logger.info("{}", logMessage);
+	}
+	
 }
--- src/main/java/pl/agh/edu/dos/PacketAnalyzer.java
+++ src/main/java/pl/agh/edu/dos/PacketAnalyzer.java
@@ -1,13 +1,8 @@
 package pl.agh.edu.dos;
 
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.lang.Integer;
-import java.util.Timer;
-import java.util.TimerTask;
 
-import org.projectfloodlight.openflow.protocol.OFMessage;
 import org.projectfloodlight.openflow.types.EthType;
 import org.projectfloodlight.openflow.types.IPv4Address;
 import org.projectfloodlight.openflow.types.IpProtocol;
@@ -16,36 +11,28 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import net.floodlightcontroller.core.FloodlightContext;
 import net.floodlightcontroller.core.IFloodlightProviderService;
-import net.floodlightcontroller.packet.ARP;
 import net.floodlightcontroller.packet.Ethernet;
 import net.floodlightcontroller.packet.IPv4;
 import net.floodlightcontroller.packet.TCP;
-import net.floodlightcontroller.packet.UDP;
 
 public class PacketAnalyzer {
 
 	private static final Logger logger = LoggerFactory.getLogger(PacketAnalyzer.class);
 	private FloodlightContext cntx;
-	private OFMessage msg;
 	protected IFloodlightProviderService floodlightProvider;
 	private Ethernet eth;
 	private IPv4 ipv4;
 	private TCP tcp;
-	private UDP udp;
 	private IPv4Address srcIP;
 	private IPv4Address dstIP;
 	private TransportPort srcPort;
 	private TransportPort dstPort;
 	private short flags;
 
-	private Map<String, Integer> counterMap;
-	private Integer simultaneousConnectionThreshold;
-	private Integer blockingTime;
+	private Map<IPv4Address, Integer> counterMap;
 
-	public PacketAnalyzer(Map<String, Integer> counterMap, Integer simultaneousConnectionThreshold, Integer blockingTime) {
+	public PacketAnalyzer(Map<IPv4Address, Integer> counterMap) {
 		this.counterMap = counterMap;
-		this.simultaneousConnectionThreshold = simultaneousConnectionThreshold;
-		this.blockingTime = blockingTime;
 	}
 
 	public void packetExtract(FloodlightContext cntx) {
@@ -85,17 +72,15 @@ public class PacketAnalyzer {
 		// logger.info("{}", logMessage);
 
 		if (dstIP.toString().equals("10.0.0.3") && flags == 2) { // if SYN flag set
-			if (counterMap.containsKey(srcIP.toString())) {
-				counterMap.put(srcIP.toString(), counterMap.get(srcIP.toString()) + 1);
+			if (counterMap.containsKey(srcIP)) {
+				counterMap.put(srcIP, counterMap.get(srcIP) + 1);
 			} else {
-				counterMap.put(srcIP.toString(), 1);
+				counterMap.put(srcIP, 1);
 			}
 			String logMessage = "New flow: Source IP: " + srcIP.toString() + "; Connection counter: "
-					+ counterMap.get(srcIP.toString()).toString();
+					+ counterMap.get(srcIP).toString();
 			logger.info("{}", logMessage);
-			if (counterMap.get(srcIP.toString()) > simultaneousConnectionThreshold) {
-				blockHostByIpAddress();
-			}
+			
 			scheduleCounterDecrementation(10000);
 		}
 
@@ -114,33 +99,12 @@ public class PacketAnalyzer {
 	}
 
 	public void decrementFlowCounter() {
-		if (counterMap.containsKey(srcIP.toString()) && counterMap.get(srcIP.toString()) > 0) {
-			counterMap.put(srcIP.toString(), counterMap.get(srcIP.toString()) - 1);
+		if (counterMap.containsKey(srcIP) && counterMap.get(srcIP) > 0) {
+			counterMap.put(srcIP, counterMap.get(srcIP) - 1);
 		}
 		String logMessage = "Counter updated for flow with Source IP: " + srcIP.toString() + "; Current connection counter: "
-				+ counterMap.get(srcIP.toString()).toString();
+				+ counterMap.get(srcIP).toString();
 		logger.info("{}", logMessage);
 	}
 	
-	public void blockHostByIpAddress() {
-		String command = "ovs-ofctl add-flow s1 \"nw_src=" + srcIP + "\",actions=drop";
-		// TODO send rest command
-		
-		String logMessage = "Host with source IP: " + srcIP.toString() + " blocked for " + blockingTime + " seconds!";
-		logger.info("{}", logMessage);
-		
-		// Schedule unblocking the host after 'blockingTime' timer expires
-		new java.util.Timer().schedule(new java.util.TimerTask() {
-			@Override
-		    public void run() {unblockHostByIpAddress();}
-		}, blockingTime*1000);
-	}
-	
-	public void unblockHostByIpAddress() {
-		String command = "ovs-ofctl del-flows s1 \"nw_src=" + srcIP + "\"";
-		// TODO send rest command
-		
-		String logMessage = "Host with source IP: " + srcIP.toString() + " unblocked.";
-		logger.info("{}", logMessage);
-	}
 }
